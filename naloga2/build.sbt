7ee5daebb7f3228440826dde66cfb466861d6084
Miha Zidar <zidarsk8@gmail.com>
2013-05-05 15:51:04 +0200

    not working

--- naloga2/build.sbt
+++ naloga2/build.sbt
@@ -5,8 +5,6 @@ version := "1.0.0"
 scalaVersion := "2.10.1"
 
 libraryDependencies  ++= Seq(
-            // other dependencies here
-            // pick and choose:
             "org.scalanlp" %% "breeze-math" % "0.2.3",
             "org.scalanlp" %% "breeze-learn" % "0.2.3",
             "org.scalanlp" %% "breeze-process" % "0.2.3",
@@ -15,8 +13,6 @@ libraryDependencies  ++= Seq(
 )
 
 resolvers ++= Seq(
-            // other resolvers here
-            // if you want to use snapshot builds (currently 0.3-SNAPSHOT), use this.
             "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
             "linter" at "http://hairyfotr.github.io/linteRepo/releases"
 )
--- naloga2/src/test/scala/functions/Integral.scala
+++ naloga2/src/test/scala/functions/Integral.scala
@@ -8,13 +8,15 @@ class IntegralTest extends FunSuite with ShouldMatchers{
   implicit val eps= 1e-15
 
   test("trapezoidal rule"){
+    
     //integrate 1/x^2 from 1 to 2
     trapezoid(10000)(x=>1/pow(x,2))(1,2) should be (0.5d +- 1e-4)
+    
     //integrate 1/x^2 +5x^5-12x^3 from 1 to 2
     trapezoid(100000)(x=>1/pow(x,2)+5*pow(x,5)-12*pow(x,3))(1,2) should be (8d +- 1e-2)
-  }
+    
+    trapezoid(100000)(x=>sin(x))(-1,1) should be (0d +- 1e-2)
 
-  test("trapezoidal: integrate e^(-x^2) x^2 from -1 to 1"){
     def f(x:Double) = pow(E,-pow(x,2))*pow(x,2)
     trapezoid(100000)(f)(-1,1) should be (0.378 +- 1e-3)
   }
@@ -23,7 +25,7 @@ class IntegralTest extends FunSuite with ShouldMatchers{
     gaussHermite(3)(x=>x*x) should be (BigDecimal(0.886226925452757) +- eps)
     // not sure gaussHermite(4)(_ pow 3) should be (BigDecimal(0) +- eps)
     gaussHermite(5)(_ pow 4) should be (BigDecimal("1.3293403881791370204736") +- eps) 
-    gaussHermite(4)(x => x + pow(sin(x.toDouble),2)) should be (BigDecimal(0.560202) +- 1e-6)
+    gaussHermite(6)(x => x + pow(sin(x.toDouble),2)) should be (BigDecimal(0.560202) +- 1e-6)
   }
 
 }
